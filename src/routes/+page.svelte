b26eff7b2b1bda6604056dd8e1e9487dba866099
rifqi <rifqihidayatmuhammad@gmail.com>
2024-09-25 21:54:54 +0800

    perbaikan home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,15 @@
 <script lang="ts">
-	// import type { PageData, ActionData } from './$types';
+// import type { PageData, ActionData } from './$types';
 
-	// export let data: PageData;
+// export let data: PageData;
 
-	// export let form: ActionData;
+// export let form: ActionData;
 </script>
 
 <section id="hadding"
     class="relative overflow-hidden bg-gradient-to-b from-blue-50 via-transparent to-transparent pb-12 pt-20 sm:pb-16 sm:pt-32 lg:pb-24 xl:pb-32 xl:pt-40">
     <div class="relative z-10">
-        <div
-            class="absolute inset-x-0 top-1/2 -z-10 flex -translate-y-1/2 justify-center overflow-hidden [mask-image:radial-gradient(50%_45%_at_50%_55%,white,transparent)]">
+        <div class="absolute inset-x-0 top-1/2 -z-10 flex -translate-y-1/2 justify-center overflow-hidden [mask-image:radial-gradient(50%_45%_at_50%_55%,white,transparent)]">
             <svg class="h-[60rem] w-[100rem] flex-none stroke-blue-600 opacity-20" aria-hidden="true">
                 <defs>
                     <pattern id="e9033f3e-f665-41a6-84ef-756f6778e6fe" width="200" height="200" x="50%" y="50%"
@@ -30,11 +29,10 @@
         <div class="mx-auto max-w-2xl text-center">
             <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
                 POLING
-                <span class="text-blue-600">KITA
-           </span>
+                <span class="text-blue-600">KITA</span>
             </h1>
             <h2 class="mt-6 text-lg leading-8 text-gray-600">
-               Kami tidak memihak pada sapapun, murni dibuat untuk ingin tahun kami bagai mana membuat poling
+                Kami tidak memihak pada siapapun, murni dibuat untuk ingin tahu kami bagaimana membuat polling.
             </h2>
             <div class="mt-10 flex items-center justify-center gap-x-6">
                 <a class="isomorphic-link isomorphic-link--internal inline-flex items-center justify-center gap-2 rounded-xl bg-blue-600 px-4 py-3 text-sm font-semibold text-white shadow-sm transition-all duration-150 hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
@@ -47,63 +45,61 @@
                 </a>
             </div>
         </div>
-        <!-- <div class="relative mx-auto mt-10 max-w-lg">
-            <img class="w-full rounded-2xl border border-gray-100 shadow" src="https://images.unsplash.com/photo-1587502536575-6dfba0a6e017" alt="">
-        </div> -->
     </div>
 </section>
+
 <section id="about">
-	<div class="flex flex-col justify-center space-y-12 text-center h-80vh w-60ch mx-auto">
-		<h2>About</h2>
-		<p>Survey kita di buat hanya untuk membantu masyarakat memberikan survey ke banyak orang.</p>
-	</div>
+    <div class="sm:flex items-center max-w-screen-xl">
+        <div class="sm:w-1/2 p-10">
+            <div class="image object-center text-center">
+                <img src="https://i.imgur.com/WbQnbas.png">
+            </div>
+        </div>
+        <div class="sm:w-1/2 p-5">
+            <div class="text">
+                <span class="text-gray-500 border-b-2 border-indigo-600 uppercase">About us</span>
+                <h2 class="my-4 font-bold text-3xl  sm:text-4xl ">Tentang <span class="text-indigo-600">POLING KITA</span>
+                </h2>
+                <p class="text-gray-700 text-sm">
+                    PolingKita adalah platform yang memudahkan pengumpulan dan analisis data polling. Kami menyediakan alat intuitif untuk membantu organisasi dan individu memahami opini publik dengan cepat dan efisien. Dengan berbagai format polling dan analisis mendalam, kami mendukung pengambilan keputusan berbasis data. Kami berkomitmen pada privasi pengguna dan transparansi, menjadikan setiap suara penting. Bergabunglah dengan PolingKita untuk berkontribusi pada perubahan positif!
+                </p>
+            </div>
+        </div>
+    </div>
 </section>
 
 <section id="survey">
-	<div class="justify-center space-y-12 text-center">
-		<h2 class="">Poling Terbaru</h2>
-		<div class="max-w-2xl mx-auto mt-24 text-left">
-			<a href="" class="flex gap-3 mt-4 bg-white border border-gray-300 rounded-xl overflow-hidden items-center justify-start">
-		
-				<div class="relative w-32 h-32 flex-shrink-0">
-					<img class="absolute left-0 top-0 w-full h-full object-cover object-center transition duration-50" loading="lazy" src="https://via.placeholder.com/150">
-				</div>
-		
-				<div class="flex flex-col gap-2 py-2">
-		
-					<p class="text-xl font-bold">Poling title</p>
-		
-					<p class="text-gray-500">
-						Description of your post/article,
-						Description of your post/article,
-					</p>		
-				</div>
-		
-			</a>
-			<a href="" class="flex gap-3 mt-4 bg-white border border-gray-300 rounded-xl overflow-hidden items-center justify-start">
-		
-				<div class="relative w-32 h-32 flex-shrink-0">
-					<img class="absolute left-0 top-0 w-full h-full object-cover object-center transition duration-50" loading="lazy" src="https://via.placeholder.com/150">
-				</div>
-		
-				<div class="flex flex-col gap-2 py-2">
-		
-					<p class="text-xl font-bold">Poling title</p>
-		
-					<p class="text-gray-500">
-						Description of your post/article,
-						Description of your post/article,
-					</p>		
-				</div>
-		
-			</a>
-		
-		</div>
-	</div>
+    <div class="justify-center space-y-12">
+        <h2 class="text-2xl font-bold text-center">Poling Terbaru</h2>
+        <!-- isi -->
+        <div class="flex flex-col p-5 lg:px-48 lg:py-11">
+            <div class="bg-gray-100 p-5 mb-10">
+                <h1 class="font-bold text-2xl mb-2">nama survey atau poling</h1>
+                <p class="my-3 text-sm">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius eaque a ipsam aliquid omnis,
+                    beatae possimus recusandae illum rem. Minima sequi voluptas repudiandae id? Quae, facere quam suscipit sed,
+                    aperiam sapiente pariatur soluta </p>
+                <button class="text-white font-semibold bg-blue-600 hover:bg-blue-800 p-2 my-1 rounded">Ikut Poling</button>
+            </div>
+            <div class="bg-gray-100 p-5 mb-10">
+                <h1 class="font-bold text-2xl mb-2">nama survey atau poling</h1>
+                <p class="my-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius eaque a ipsam aliquid omnis,
+                    beatae possimus recusandae illum rem. Minima sequi voluptas repudiandae id? Quae, facere quam suscipit sed,
+                    aperiam sapiente pariatur soluta </p>
+                <button class="text-white font-semibold bg-blue-600 hover:bg-blue-800 p-2 my-1 rounded">Lakukan Poling</button>
+            </div>
+            <div class="bg-gray-100 p-5 mb-10">
+                <h1 class="font-bold text-2xl mb-2">nama survey atau poling</h1>
+                <p class="my-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius eaque a ipsam aliquid omnis,
+                    beatae possimus recusandae illum rem. Minima sequi voluptas repudiandae id? Quae, facere quam suscipit sed,
+                    aperiam sapiente pariatur soluta </p>
+                <button class="text-white font-semibold bg-blue-600 hover:bg-blue-800 p-2 my-1 rounded">Lakukan Poling</button>
+            </div>
+        </div>
+    </div>
 </section>
 
 <style scoped>
-	section p {
-		--at-apply: text-2xl text-[#888] font-semibold;
-	}
+    /* section p {
+        --at-apply: text-2xl text-[#888] font-semibold;
+    } */
 </style>
