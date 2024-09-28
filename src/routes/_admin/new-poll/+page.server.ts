91076fdea903ccc36bfa06a424cf96ce49e4f0e2
ahmadyasser72 <ahmadyasser72@smk.belajar.id>
2024-09-28 13:06:31 +0800

    gunakan environment variable untuk akses server pockethost

--- src/routes/_admin/new-poll/+page.server.ts
+++ src/routes/_admin/new-poll/+page.server.ts
@@ -3,15 +3,20 @@ import type { Actions } from './$types';
 import { fail } from '@sveltejs/kit';
 
 export const actions = {
-	default: async ({ request, locals }) => {
+	default: async ({
+		request,
+		locals: {
+			collection: { polling }
+		}
+	}) => {
 		const formData = await request.formData();
 
 		const namaRoute = formData.get('nama_route') as string;
 		try {
-			await locals.pb.collection('polling').getFirstListItem(`nama_route="${namaRoute}"`);
+			await polling.getFirstListItem(`nama_route="${namaRoute}"`);
 		} catch (error) {
 			if (error instanceof ClientResponseError && error.status === 404) {
-				await locals.pb.collection('polling').create(formData);
+				await polling.create(formData);
 				return;
 			}
 
--- src/routes/polling/[id]/+page.server.ts
+++ src/routes/polling/[id]/+page.server.ts
@@ -1,13 +1,12 @@
-import PocketBase, { ClientResponseError, type RecordModel } from 'pocketbase';
+import { ClientResponseError } from 'pocketbase';
 import type { Actions, PageServerLoad } from './$types';
 import { error } from '@sveltejs/kit';
-import type { Poll, PollResult } from '$lib/types';
 
 export const load: PageServerLoad = async ({ params, locals }) => {
 	try {
-		const record = await getPoll(locals.pb, params.id);
+		const record = await getPoll(locals, params.id);
 		const { id: pollId, nama, pertanyaan, daftar_pilihan, daftar_gambar_pilihan } = record;
-		const hasil = await getPollResult(locals.pb, pollId);
+		const hasil = await getPollResult(locals, pollId);
 
 		return {
 			hasil,
@@ -32,20 +31,23 @@ export const load: PageServerLoad = async ({ params, locals }) => {
 export const actions: Actions = {
 	default: async ({ request, params, locals }) => {
 		const formData = await request.formData();
-		const { id: pollId, daftar_pilihan } = await getPoll(locals.pb, params.id);
+		const { id: pollId, daftar_pilihan } = await getPoll(locals, params.id);
 		formData.append('polling', pollId);
 
-		await locals.pb.collection('hasil_polling').create(formData);
+		await locals.collection.hasil_polling.create(formData);
 		return { pilihan: daftar_pilihan[Number(formData.get('index_pilihan'))] };
 	}
 };
 
-const getPoll = (pb: PocketBase, route: string) =>
-	pb.collection('polling').getFirstListItem<RecordModel & Poll>(`nama_route="${route}"`);
+const getPoll = ({ collection: { polling } }: App.Locals, route: string) =>
+	polling.getFirstListItem(`nama_route="${route}"`);
 
-const getPollResult = async (pb: PocketBase, id: string) => {
-	const { daftar_pilihan } = await pb.collection('polling').getOne<Poll>(id);
-	const records = await pb.collection('hasil_polling').getFullList<PollResult>({
+const getPollResult = async (
+	{ collection: { polling, hasil_polling } }: App.Locals,
+	id: string
+) => {
+	const { daftar_pilihan } = await polling.getOne(id);
+	const records = await hasil_polling.getFullList({
 		filter: `polling="${id}"`
 	});
 
