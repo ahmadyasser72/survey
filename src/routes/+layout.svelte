288109b3cf7b37c8ad04345aae57c19c7174bbcb
ahmadyasser72 <ahmadyasser72@smk.belajar.id>
2024-10-06 09:43:06 +0800

    perbaiki navbar mobile

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,15 +11,19 @@
 	onMount(() => {
 		setProgressBar(progressbar);
 	});
+
+	let showMobileMenu = false;
+	$: mobileMenuClasses = showMobileMenu ? 'max-h-64' : 'max-h-0 <md:-translate-x-100%';
 </script>
 
 <ProgressBar bind:this={progressbar} class="text-gradient" />
+
 <header class="lg:px-16 px-4 bg-white flex flex-wrap items-center py-4 shadow-md">
 	<div class="flex-1 flex justify-between items-center">
 		<a href="/" class="text-xl">Pilihanku</a>
 	</div>
 
-	<label for="menu-toggle" class="pointer-cursor md:hidden block">
+	<label for="menu-toggle" class="cursor-pointer md:hidden">
 		<svg
 			class="fill-current text-gray-900"
 			xmlns="http://www.w3.org/2000/svg"
@@ -31,19 +35,21 @@
 			<path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
 		</svg>
 	</label>
-	<input class="hidden" type="checkbox" id="menu-toggle" />
+	<input
+		on:change={() => (showMobileMenu = !showMobileMenu)}
+		class="hidden"
+		type="checkbox"
+		id="menu-toggle"
+	/>
 
-	<div class="hidden md:flex md:items-center md:w-auto w-full" id="menu">
-		<nav>
-			<ul class="md:flex items-center justify-between text-base text-gray-700 pt-4 md:pt-0">
-				<li><a class="md:p-4 py-3 px-0 block" href="/">Home</a></li>
-				<li><a class="md:p-4 py-3 px-0 block" href="/#about">AboutUs</a></li>
-				<li><a class="md:p-4 py-3 px-0 block" href="/polling">Polling</a></li>
-				<!-- <li><a class="md:p-4 py-3 px-0 block" href="#">Blog</a></li> -->
-				<li><a class="md:p-4 py-3 px-0 block md:mb-0 mb-2" href="/#saran">Saran</a></li>
-			</ul>
-		</nav>
-	</div>
+	<nav id="menu" class="<md:w-full">
+		<ul class="md:flex items-center justify-between text-base text-gray-700 {mobileMenuClasses}">
+			<li><a class="md:p-4 py-3 px-0 block" href="/">Home</a></li>
+			<li><a class="md:p-4 py-3 px-0 block" href="/#about">About</a></li>
+			<li><a class="md:p-4 py-3 px-0 block" href="/polling">Polling</a></li>
+			<li><a class="md:p-4 py-3 px-0 block" href="/#saran">Saran</a></li>
+		</ul>
+	</nav>
 </header>
 
 <div>
@@ -90,5 +96,11 @@
 	:global(.text-gradient) {
 		background: linear-gradient(to right, #0132d9, #e64c54);
 		color: transparent;
+	}
+
+	nav#menu ul {
+		transition-property: max-height, transform;
+		transition-duration: 300ms, 334ms;
+		transition-timing-function: ease-out, ease-in-out;
 	}
 </style>
